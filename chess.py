849ce869b35537758eb4999782c86d62a267e79c
horrible <terribleapps@gmail.com>
2021-12-26 22:55:55 -0800

    checkmate is almost working

--- chess.py
+++ chess.py
@@ -290,7 +290,7 @@ def kmask(brd,msk,pm,bm,pc,plr):
     return brd,msk,pc,plr,pm,bm
 
 def pmask(brd,msk,pm,bm,pc,plr):
-    print("inside rmask")
+    print("inside pmask")
     othplr = 2 if plr==1 else 1
     x,y,pm,pc,brd,msk = updatebrd(msk,brd,pc,pm)
     if plr==1:
@@ -301,17 +301,34 @@ def pmask(brd,msk,pm,bm,pc,plr):
             msk[x+1,y-1]=True
 
     elif plr==2:
-        msk[x-1,y][pm[x-1,y]==0]=True
+        #msk[x-1,y][pm[x-1,y]==0]=True
+        msk[x-1,y] = True if pm[x-1,y]==0 else False
         if pm[x-1,y+1]==othplr:
             msk[x-1,y+1]=True
         if pm[x-1,y-1]==othplr:
             msk[x-1,y-1]=True
+    #pdb.set_trace()
     msk[~bm]=False
     brd,pm=plrmat(brd,pm)
     disp(brd,bm,pm,msk)
     return brd,msk,pc,plr,pm,bm
 
-def test_knight():
+def getmsk(brd,msk,pm,bm,pc,plr):
+    if 'r' in pc[0]:
+        brd,msk,pc,plr,pm,bm=rmask(brd,msk,pm,bm,pc,plr)
+    elif 'n' in pc[0]:
+        brd,msk,pc,plr,pm,bm=nmask(brd,msk,pm,bm,pc,plr)
+    elif 'b' in pc[0]:
+        brd,msk,pc,plr,pm,bm=bmask(brd,msk,pm,bm,pc,plr)
+    elif 'q' in pc[0]:
+        brd,msk,pc,plr,pm,bm=qmask(brd,msk,pm,bm,pc,plr)
+    elif 'k' in pc[0]:
+        brd,msk,pc,plr,pm,bm=kmask(brd,msk,pm,bm,pc,plr)
+    elif 'p' in pc[0]:
+        brd,msk,pc,plr,pm,bm=pmask(brd,msk,pm,bm,pc,plr)
+    return brd,msk,pc,plr,pm,bm
+
+def t_knight():
     #testing knight
     msk,brd,pm,bm=chessbrd()
     brd[lo+3][lo+2]='n11'
@@ -321,7 +338,7 @@ def test_knight():
     brd,msk,pc,plr,pm,bm=nmask(brd,msk,pm,bm,pc,plr)
     disp(brd,bm,pm,msk)
 
-def test_rook():
+def t_rook():
     #testing rook
     msk,brd,pm,bm=chessbrd()
     pc='r11'
@@ -334,7 +351,7 @@ def test_rook():
     brd,msk,pc,plr,pm,bm=rmask(brd,msk,pm,bm,pc,plr)
     disp(brd,bm,pm,msk)
 
-def test_bishop():
+def t_bishop():
     #testing bishop
     msk,brd,pm,bm=chessbrd()
     pc='b11'
@@ -347,7 +364,7 @@ def test_bishop():
     brd,msk,pc,plr,pm,bm=bmask(brd,msk,pm,bm,pc,plr)
     disp(brd,bm,pm,msk)
 
-def test_queen():
+def t_queen():
     #testing bishop
     msk,brd,pm,bm=chessbrd()
     pc='q11'
@@ -361,7 +378,7 @@ def test_queen():
     brd,msk,pc,plr,pm,bm=qmask(brd,msk,pm,bm,pc,plr)
     disp(brd,bm,pm,msk)    
 
-def test_king():
+def t_king():
     #testing king
     msk,brd,pm,bm=chessbrd()
     pc='k11'
@@ -375,7 +392,7 @@ def test_king():
     brd,msk,pc,plr,pm,bm=kmask(brd,msk,pm,bm,pc,plr)
     disp(brd,bm,pm,msk) 
 
-def test_pawn():
+def t_pawn():
     #testing pawn
     msk,brd,pm,bm=chessbrd()
     pc='p12'
@@ -390,23 +407,133 @@ def test_pawn():
     brd,msk,pc,plr,pm,bm=pmask(brd,msk,pm,bm,pc,plr)
     disp(brd,bm,pm,msk) 
 
-def test_checkmate():
+def dbgchk(mskdict,pmdict,brd,keyOI):
+    print(brd[lo:hi,lo:hi])
+    print()
+    print(mskdict[keyOI][lo:hi,lo:hi].astype('int8'))
+    print()
+    print(pmdict[keyOI][lo:hi,lo:hi].astype('int8'))
+    print()
+    print(mskdict.keys())
+    print()
+
+
+def checkmate(brd,msk,pm,bm,pc,plr):
+    print("inside checkmate")
+    othplr = 2 if plr==1 else 1
+    #x,y,pm,pc,brd,msk = updatebrd(msk,brd,pc,pm)
+    othplpcs = brd[pm==othplr]
+    disp(brd,bm,pm,msk)
+    mskdict=dict()
+    pmdict =dict()
+    for pcoi in othplpcs:
+        msk[:,:]=0
+        pm[:,:]=0
+        brd,pm=plrmat(brd,pm)
+        tbrd,tmsk,tpc,tplr,tpm,bm=getmsk(brd,msk,pm,bm,pcoi,othplr)
+        disp(brd,bm,pm,msk)   
+        mskdict[pcoi]=tmsk.copy()
+        pmdict[pcoi]=pm.copy()
+        #print(mskdict['n22'][lo:hi,lo:hi].astype('int8'))
+        #print(brd[lo:hi,lo:hi])
+        #print(pmdict['n22'][lo:hi,lo:hi].astype('int8'))
+        #print(mskdict.keys())
+    otherplmsk=np.sum([x for x in mskdict.values()],axis=0).astype('bool') # combined mask of all othrplr pieces
+    msk[:,:]=0
+    pm[:,:]=0
+    brd,pm=plrmat(brd,pm)
+    brd,msk,pc,plr,pm,bm=kmask(brd,msk,pm,bm,pc,plr)
+    print("\n\n")
+    print("\npm")
+    print(pm[lo:hi,lo:hi].astype('int8'))
+    print("\notherplmsk")
+    print(otherplmsk[lo:hi,lo:hi].astype('int8'))
+    print("\nmsk")
+    print(msk[lo:hi,lo:hi].astype('int8'))
+    print("\ncheckmate msk")
+    chmsk=~otherplmsk & msk
+    print(chmsk[lo:hi,lo:hi].astype('int8'))
+    print("\nbrd")
+    print(brd[lo:hi,lo:hi])
+    pdb.set_trace()
+    return brd,msk,pc,plr,pm,bm,chmsk
+
+def checkmate2(brd,msk,pm,bm,pc,plr,chmsk):
+    chblist=list()
+    idx=0
+    for ir,ic in zip(np.where(chmsk==True)[0],np.where(chmsk==True)[1]):
+        tbrd=brd.copy();tmsk=msk.copy();tpm=pm.copy();tbm=bm.copy();tchmsk=chmsk.copy()
+        tpc=pc;tplr=plr
+        tmsk[:,:]=0
+        tpm[:,:]=0
+        tx,ty,tpm,tpc,tbrd,tmsk = updatebrd(tmsk,tbrd,tpc,tpm)
+        tbrd[ir,ic]=tpc
+        tbrd[tx,ty]='000'
+        tbrd,tpm=plrmat(tbrd,tpm)
+        tbrd,tmsk,tpc,tplr,tpm,tbm,tchmsk=checkmate(tbrd,tmsk,tpm,tbm,tpc,tplr)
+        chblist.append(tchmsk)
+    #pdb.set_trace()
+    if np.sum(chblist)>0:
+        return False
+    else:
+        return True
+    
+
+
+def t_checkmate_false():
     #testing pawn
     msk,brd,pm,bm=chessbrd()
     pc='k11'
     plr=1
     brd[lo+3][lo+2]='p12'
     brd[lo+1][lo+2]=\
-    brd[lo+6][lo+3]=\
     brd[lo+6][lo+5]=\
+    brd[lo+7][lo+7]=\
+    brd[lo+7][lo+0]=\
     '000'
     brd[lo+0][lo+4]='000'
-    brd[lo+4][lo+3]='k11'
-    brd[lo+4][lo+1]='p21'
+    brd[lo+4][lo+3]='k11' # place king in middle
+    brd[lo+4][lo+1]='p21' # 
+    brd[lo+6][lo+5]='b22';brd[lo+7][lo+5]='p25'
+    brd[lo+5][lo+0]='r21' #moving rook
+    brd[lo+4][lo+6]='r22'
     brd[lo+6][lo+1]='000'
     brd,pm=plrmat(brd,pm)
-    brd,msk,pc,plr,pm,bm=pmask(brd,msk,pm,bm,pc,plr)
+    brd,msk,pc,plr,pm,bm,chmsk=checkmate(brd,msk,pm,bm,pc,plr)
+    chkbool=checkmate2(brd,msk,pm,bm,pc,plr,chmsk)
     disp(brd,bm,pm,msk)     
+    print("Checkmate bool:"+str(chkbool))
+
+def t_checkmate_true():
+    #testing pawn
+    msk,brd,pm,bm=chessbrd()
+    pc='k11'
+    plr=1
+    brd[lo+3][lo+2]='p12'
+    brd[lo+1][lo+2]=\
+    brd[lo+6][lo+5]=\
+    brd[lo+7][lo+7]=\
+    brd[lo+7][lo+0]=\
+    brd[lo+6][lo+3]=\
+    brd[lo+7][lo+2]=\
+    brd[lo+7][lo+6]=\
+    '000'
+    brd[lo+0][lo+4]='000'
+    brd[lo+4][lo+3]='k11' # place king in middle
+    brd[lo+4][lo+1]='p21' # 
+    brd[lo+6][lo+5]='b22';brd[lo+7][lo+5]='p25'
+    brd[lo+3][lo+1]='b21'
+    brd[lo+6][lo+1]='n22'
+    brd[lo+5][lo+0]='r21' #moving rook
+    brd[lo+4][lo+6]='r22'
+    brd[lo+5][lo+5]='n22'
+    brd,pm=plrmat(brd,pm)
+    brd,msk,pc,plr,pm,bm,chmsk=checkmate(brd,msk,pm,bm,pc,plr)
+    chkbool=checkmate2(brd,msk,pm,bm,pc,plr,chmsk)
+    disp(brd,bm,pm,msk)     
+    print("Checkmate bool:"+str(chkbool))
+
+
 
 def main():
     if len(sys.argv)>1:
@@ -414,13 +541,14 @@ def main():
             dbg=True
         else:
             dbg=False
-    #test_knight()
-    #test_rook()
-    #test_bishop()
-    #test_queen()
-    #test_king()
-    #test_pawn()
-    test_checkmate()
+    #t_knight()
+    #t_rook()
+    #t_bishop()
+    #t_queen()
+    #t_king()
+    #t_pawn()
+    #t_checkmate_false()
+    t_checkmate_true()
 
 
 if __name__ == "__main__":
