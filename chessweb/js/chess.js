b3130ce187bb85c53ac3dad19ea6b6fd051915e1
horrible <horribleapps@gmail.com>
2022-01-02 11:25:31 -0800

    working on js version of chessw

---
+++ chessweb/js/chess.js
@@ -0,0 +1,98 @@
+
+//code: https://codepen.io/viethoang012/pen/xRNgyM
+
+function createBoard() {
+    let dv=document.getElementsByTagName('div');
+    let idx=2;
+    let pieces =    [["♜","♞","♝","♛","♚","♝","♞","♜",
+                      "♟","♟","♟","♟","♟","♟","♟","♟",],
+                     ["♙","♙","♙","♙","♙","♙","♙","♙",
+                      "♖","♘","♗","♕","♔","♗","♘","♖",]];
+    for (var i = 0;i<pieces.length;i++) {
+        for(var j=0;j<pieces[i].length;j++){
+            if (i==0){
+            dv[j+2].innerText=pieces[i][j];
+            }
+            else {
+                dv[j+50].innerText=pieces[i][j];
+            }
+        }
+    console.log(dv[idx].innerText);
+    }
+}
+
+function createButton
+
+createBoard();
+
+
+/*var dv = document.getElementsByTagName('div');
+var idx=2;
+console.log(dv[idx].innerText);
+
+var temp=dv[idx];
+var container = temp;
+var dragItem=temp;
+var active = false;
+var currentX;
+var currentY;
+var initialX;
+var initialY;
+var xOffset = 0;
+var yOffset = 0;
+
+
+temp.addEventListener("touchstart", dragStart, false);
+temp.addEventListener("touchend", dragEnd, false);
+temp.addEventListener("touchmove", drag, false);
+
+temp.addEventListener("mousedown", dragStart, false);
+temp.addEventListener("mouseup", dragEnd, false);
+temp.addEventListener("mousemove", drag, false);
+
+function dragStart(e) {
+  if (e.type === "touchstart") {
+    initialX = e.touches[0].clientX - xOffset;
+    initialY = e.touches[0].clientY - yOffset;
+  } else {
+    initialX = e.clientX - xOffset;
+    initialY = e.clientY - yOffset;
+  }
+  
+
+  if (e.target === temp) {
+    active = true;
+  }
+}
+
+function dragEnd(e) {
+  initialX = currentX;
+  initialY = currentY;
+  console.log(e);
+ 
+  active = false;
+}
+
+function drag(e) {
+  if (active) {
+  
+    e.preventDefault();
+  
+    if (e.type === "touchmove") {
+      currentX = e.touches[0].clientX - initialX;
+      currentY = e.touches[0].clientY - initialY;
+    } else {
+      currentX = e.clientX - initialX;
+      currentY = e.clientY - initialY;
+    }
+
+    xOffset = currentX;
+    yOffset = currentY;
+
+    setTranslate(currentX, currentY, temp);
+  }
+}
+
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+}*/
