76d5d7197ba619a0b3484d3a68d6bb8e5b99b76d
horrible <horribleapps@gmail.com>
2022-01-08 23:27:06 -0800

    now added things for moving pieces

--- chessweb/js/chess.js
+++ chessweb/js/chess.js
@@ -1,68 +1,69 @@
 
 //code: https://codepen.io/viethoang012/pen/xRNgyM
 
-let plrOne = {
-                'r11':["♜",1],
-                'n11':["♞",2],
-                'b11':["♝",3],
-                'q11':["♛",4],
-                'k11':["♚",5],
-                'b12':["♝",6],
-                'n12':["♞",7],
-                'r12':["♜",8],
-                'p11':["♟",9],
-                'p12':["♟",10],
-                'p13':["♟",11],
-                'p14':["♟",12],
-                'p15':["♟",13],
-                'p16':["♟",14],
-                'p17':["♟",15],
-                'p18':["♟",16],
-                };
-
-let plrTwo = {
-                    'p21':["♙",49],
-                    'p22':["♙",50],
-                    'p23':["♙",51],
-                    'p24':["♙",52],
-                    'p25':["♙",53],
-                    'p26':["♙",54],
-                    'p27':["♙",55],
-                    'p28':["♙",56],
-                    'r21':["♖",57],
-                    'n21':["♘",58],
-                    'b21':["♗",59],
-                    'q21':["♕",60],
-                    'k21':["♔",61],
-                    'b22':["♗",62],
-                    'n22':["♘",63],
-                    'r22':["♖",64],
+let playerpcs = {
+                    'r11':["♜",1,1],
+                    'n11':["♞",2,1],
+                    'b11':["♝",3,1],
+                    'q11':["♛",4,1],
+                    'k11':["♚",5,1],
+                    'b12':["♝",6,1],
+                    'n12':["♞",7,1],
+                    'r12':["♜",8,1],
+                    'p11':["♟",9,1],
+                    'p12':["♟",10,1],
+                    'p13':["♟",11,1],
+                    'p14':["♟",12,1],
+                    'p15':["♟",13,1],
+                    'p16':["♟",14,1],
+                    'p17':["♟",15,1],
+                    'p18':["♟",16,1],
+                    'p21':["♙",49,2],
+                    'p22':["♙",50,2],
+                    'p23':["♙",51,2],
+                    'p24':["♙",52,2],
+                    'p25':["♙",53,2],
+                    'p26':["♙",54,2],
+                    'p27':["♙",55,2],
+                    'p28':["♙",56,2],
+                    'r21':["♖",57,2],
+                    'n21':["♘",58,2],
+                    'b21':["♗",59,2],
+                    'q21':["♕",60,2],
+                    'k21':["♔",61,2],
+                    'b22':["♗",62,2],
+                    'n22':["♘",63,2],
+                    'r22':["♖",64,2],
                     };
+cemetary={};
+pcdict = {
+  'n':knightMove,
+  'b':bishopMove,
+  'p':pawnMove,
+  'r':rookMove,
+  'k':kingMove,
+  'q':queenMove,
+}
 
 let availMoves = [];
 
-let poi=0; //piece of interest index
+let pcidx=0; //piece of interest index
 let moveList=[];
-player=1;
+let player=1;
+let movebool=false;
+let keyoi='';
 
 function populateBoard() {
     let dv=document.getElementsByTagName('div');
     refreshGrid();
-    for (let i in plrOne) {
-        dv[plrOne[i][1]].innerText=plrOne[i][0];
-        //console.log(plrOne[i]);
-    }
-
-    for (let j in plrTwo) {
-        dv[plrTwo[j][1]].innerText=plrTwo[j][0];
-        //console.log(plrTwo[j]);
+    for (let i in playerpcs) {
+        dv[playerpcs[i][1]].innerText=playerpcs[i][0];;
     }
 }
 
 function refreshGrid() {
     let dv=document.getElementsByTagName('div');
     for (let v=1;v<65;v++) {
-        //debugger;
         startclr = (parseInt( ((v-1)/8) % 2)==0 ) ? 'white':'gray';
         endclr = (parseInt( ((v-1)/8) % 2)==0 ) ? 'gray':'white';
         dv[v].style.backgroundColor = (v%2==0 ) ? startclr:endclr;
@@ -70,33 +71,107 @@ function refreshGrid() {
     }
 }
 
-function availableMoves(z,player){
+function getKey(idx){
+  for (let i in playerpcs) {
+    if(playerpcs[i][1] == idx){
+      return i
+    }
+  }
+  return -1;
+}
+
+function clearBoard(){
+  let dv=document.getElementsByTagName('div');
+  for (let v=1;v<dv.length;v++) {
+      dv[v].innerText='';
+  }
+}
+
+function updateBoard() {
+  let dv=document.getElementsByTagName('div');
+  refreshGrid();
+  clearBoard();
+  for (let i in playerpcs) {
+    dv[playerpcs[i][1]].innerText=playerpcs[i][0]
+  }
+}
+
+function availableMoves(z,poi){
     //find all the available moves
     refreshGrid();
+    pcidx=z;
     let dv=document.getElementsByTagName('div');
-    availMoves=[z+8,z+16];
-    for (let k=0;k<availMoves.length;k++) {
-      dv[availMoves[k]].style.backgroundColor='red';
-      dv[availMoves[k]].style.opacity=0.5;
+    key=getKey(z);
+    //if (key==-1){console.log("No key found: "+key);}
+    tmp = (key==-1) ? null:pcdict[key[0]](key);
+    if(dv[z].innerText != ''){
+      for (let k=0;k<availMoves.length;k++) {
+        dv[availMoves[k]].style.backgroundColor='red';
+        dv[availMoves[k]].style.opacity=0.5;
+      }
     }
 }
 
-function checkpc(tdv,idx,player) {
+function killpc(idx) {
+  let dv=document.getElementsByTagName('div');
+  for (let i in playerpcs) {
+    if((playerpcs[i][1] == idx) & (playerpcs[i][2] != player)){
+      cemetary[i]=playerpcs[i];
+      delete playerpcs[i];
+    }
+  }
+}
+
+function updatedict(idx){
+  let dv=document.getElementsByTagName('div');
+
+  for (let i in playerpcs) {
+    if(playerpcs[i][2]==player) {
+      if(playerpcs[i][1]==pcidx){
+        if(playerpcs[i][1])
+        killpc(idx);
+        playerpcs[i][1]=idx;
+        dv[playerpcs[i][1]].innerText=playerpcs[i][0];
+      }
+    }
+  }
+}
+
+function checkplayerpc(idx){
+  
+  for (let i in playerpcs) {
+    if(playerpcs[i][2]==player) {
+      if((playerpcs[i][2]==player) & (playerpcs[i][1]==idx) ){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function checkpc(tdv,idx) {
   let dv=document.getElementsByTagName('div');
   if(player==1){
-    for (let i in plrOne) {
-        if(dv[plrOne[i][1]].innerText==tdv.innerText){
-          availableMoves(idx,player);
-        }
+    if(dv[idx].style.backgroundColor=='red'){
+      updatedict(idx);
+      updateBoard();
+      player=2;
+    }
+    else if ((dv[idx].innerText==tdv.innerText) & (checkplayerpc(idx)==true)){
+      availableMoves(idx);
     }
   }
-  if(player==2) {
-    for (let j in plrTwo) {
-      if(dv[plrOne[j][1]].innerText==tdv.innerText){
-        availableMoves(idx,player);
-        }
+  else if(player==2){
+    if(dv[idx].style.backgroundColor=='red'){
+      updatedict(idx);
+      updateBoard();
+      player=1;
+    }
+    else if((dv[idx].innerText==tdv.innerText) & (checkplayerpc(idx)==true)){
+      availableMoves(idx);
     }
   }
+  console.log(pcidx)
 }
 
 function mouseclick(event) {
@@ -120,75 +195,91 @@ function mouseclick(event) {
 
 populateBoard();
 document.addEventListener("click",mouseclick);
-//createButtons();
-
-/*var dv = document.getElementsByTagName('div');
-var idx=2;
-console.log(dv[idx].innerText);
-
-var temp=dv[idx];
-var container = temp;
-var dragItem=temp;
-var active = false;
-var currentX;
-var currentY;
-var initialX;
-var initialY;
-var xOffset = 0;
-var yOffset = 0;
 
 
-temp.addEventListener("touchstart", dragStart, false);
-temp.addEventListener("touchend", dragEnd, false);
-temp.addEventListener("touchmove", drag, false);
+/* BELOW HERE ARE FIGURING ARE THE AVAILABLE OPTIONS FOR A CERTAIN PIECE*/
 
-temp.addEventListener("mousedown", dragStart, false);
-temp.addEventListener("mouseup", dragEnd, false);
-temp.addEventListener("mousemove", drag, false);
-
-function dragStart(e) {
-  if (e.type === "touchstart") {
-    initialX = e.touches[0].clientX - xOffset;
-    initialY = e.touches[0].clientY - yOffset;
-  } else {
-    initialX = e.clientX - xOffset;
-    initialY = e.clientY - yOffset;
-  }
-  
-
-  if (e.target === temp) {
-    active = true;
-  }
-}
-
-function dragEnd(e) {
-  initialX = currentX;
-  initialY = currentY;
-  console.log(e);
- 
-  active = false;
-}
-
-function drag(e) {
-  if (active) {
-  
-    e.preventDefault();
-  
-    if (e.type === "touchmove") {
-      currentX = e.touches[0].clientX - initialX;
-      currentY = e.touches[0].clientY - initialY;
-    } else {
-      currentX = e.clientX - initialX;
-      currentY = e.clientY - initialY;
+function removeBoundaries(loclist){
+  primedList=[];
+  for(let l=0;l<loclist.length;l++){
+    r=loclist[l][0];
+    c=loclist[l][1];
+    if ((r<8) & (r>=0) & (c<8) & (c>=0)) {
+      primedList.push([r,c]);
     }
-
-    xOffset = currentX;
-    yOffset = currentY;
-
-    setTranslate(currentX, currentY, temp);
   }
+  return primedList;
 }
 
-function setTranslate(xPos, yPos, el) {
-  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-}*/
+function checkPieces(boundedList){
+  keepOpponentList=[];
+  let dv=document.getElementsByTagName('div');
+  for(let l=0;l<boundedList.length;l++){
+    r=boundedList[l][0];
+    c=boundedList[l][1];
+    idx=8*parseInt(r)+c;
+    key=getKey(idx);
+    if(dv[idx].innerText!=''){
+      if (playerpcs[key][2]!=player){
+        keepOpponentList.push([r,c]);
+      }
+    }
+    else {
+      keepOpponentList.push([r,c]);
+    }
+  }
+  return keepOpponentList;
+}
+
+function convertToIdx(rcList){
+  finalList=[];
+  for(let l=0;l<rcList.length;l++){
+    r=boundedList[l][0];
+    c=boundedList[l][1];
+    idx=8*parseInt(r)+c;
+    finalList.push(idx);
+  }
+  return finalList;
+}
+
+function knightMove(i){
+  d=playerpcs[i];
+  loc=d[1];
+  r=parseInt(loc/8);
+  c=loc%8;
+  loclist= [
+    [r+2,c+1],
+    [r+2,c-1],
+    [r-2,c+1],
+    [r-2,c-1],
+    [r-1,c+2],
+    [r-1,c-2],
+    [r+1,c+2],
+    [r+1,c-2],
+    ]
+  boundedList=removeBoundaries(loclist);
+  keepOpponentList=checkPieces(boundedList);
+  finalList=convertToIdx(keepOpponentList);
+  availMoves=finalList;
+}
+
+function bishopMove(i){
+  d=playerpcs[i];
+  loc=d[1];
+}
+function pawnMove(i){
+  d=playerpcs[i];
+  loc=d[1];
+}
+function rookMove(i){
+  d=playerpcs[i];
+  loc=d[1];
+}
+function kingMove(i){
+  d=playerpcs[i];
+  loc=d[1];
+}
+function queenMove(i){
+  d=playerpcs[i];
+  loc=d[1];
+}
